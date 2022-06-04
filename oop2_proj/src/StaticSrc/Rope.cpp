03d6432d6bf9ab5def5562bce53ffb286b79f392
Racheli <Racheli@DESKTOP-1GVI58U>
2022-06-04 18:17:44 +0300

    adding rope

--- oop2_proj/src/StaticSrc/Rope.cpp
+++ oop2_proj/src/StaticSrc/Rope.cpp
@@ -2,7 +2,7 @@
 #include "StaticInclude\Rope.h"
 
 Rope::Rope(sf::Vector2f pos)
-	: StaticObj(sf::Vector2f(BLOCK_SIZE / 1.2, BLOCK_SIZE), pos)
+	: StaticObj(sf::Vector2f(BLOCK_SIZE/3 , BLOCK_SIZE*2), pos)
 {
 	m_shape.setTexture(FileManager::instance().getSharedStaticTexture(ROPE));
 }
