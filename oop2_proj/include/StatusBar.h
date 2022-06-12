6def6bc33c43da6f168e7fa1eccd804dc6c8ca80
Racheli <Racheli@DESKTOP-1GVI58U>
2022-06-12 23:48:36 +0300

    ss

--- oop2_proj/include/StatusBar.h
+++ oop2_proj/include/StatusBar.h
@@ -10,57 +10,42 @@ public:
 	StatusBar();
 	~StatusBar() {};
 
-	void updateTime();
-	void updateLevel(bool);
+	void updateLevel(const bool);
 	int getLevel() const;
-	void setLevel(int);
-	void setMaxDiamonds(int max) { m_maxDiamonds=max; }
-	float getTime() const;
-	void resetTimer();
-	void setTime(int);
+	void setLevel(const int);
+	void setMaxDiamonds(const int max) { m_maxDiamonds=max; }
 	void draw(sf::RenderWindow&,const int ,const int,const int);
 	void resetNumOfLevel();
 	bool containsMusicIcon(const sf::Event&) const;
-	void setMusicIcon(bool);
+	void setMusicIcon(const bool);
 	bool containsStopAndPlayIcon(const sf::Event&) const;
-	void setStopAndPlayIcon(bool);
+	void setStopAndPlayIcon(const bool);
 	bool containsRestartIcon(const sf::Event&) const;
-	void updatePos(sf::Vector2f);
+	void updatePos(const sf::Vector2f);
 
 private:
 	void setLevelText();
 	void setDiamondCounter();
-	void setTimeText();
 	void setMusicIcon();
 	void setStopIcon();
 	void setRestartIcon();
-	void updateTimePassText();
 	void updateDiamondText(const int);
 	void setCurrLifeRect();
-	void setCurrPowerRect();//50.f, 8.f
-	void updateLifeAndPower(int power,int life);
+	void setCurrPowerRect();
+	void updateLifeAndPower(const int power, const int life);
 
 
-	sf::Clock m_timer;
-	float m_minutes;
-	float m_seconds;
 	int m_level;
 	int m_maxDiamonds = 0;
-
-	sf::Text m_timeText;
 	sf::Text m_levelText;
 	sf::Text m_diamondText;
 	sf::Font m_font;
-
-
 	sf::Sprite m_stopAndPlayIcon;
 	sf::Sprite m_musicIcon;
 	sf::Sprite m_resetIcon;
 	sf::Sprite m_lifeIcon;
 	sf::Sprite m_powerIcon;
 	sf::Sprite m_diamondIcon;
-
-
 	sf::RectangleShape
 		m_currLife,
 		m_currPower,
--- oop2_proj/src/StatusBar.cpp
+++ oop2_proj/src/StatusBar.cpp
@@ -7,62 +7,26 @@
 //--------------------------------------------------
 //constructor
 
-StatusBar::StatusBar() : m_level(0), m_minutes(0), m_seconds(0)
+StatusBar::StatusBar() : m_level(0)
 {
 	this->m_font = (*(FileManager::instance().getFont()));
 
-	//setCurrPlayerText();
 	setCurrPowerRect();
 	setCurrLifeRect();
 	setDiamondCounter();
 	setLevelText();
-	setTimeText();
 	setMusicIcon();
 	setStopIcon();
 	//setRestartIcon();
 }
 //------------------------------------------
 
-void StatusBar::updateTime()
-{
-		this->m_seconds += this->m_timer.getElapsedTime().asSeconds();
-		if (m_seconds >= 60)
-		{
-			this->m_seconds = 0;
-			this->m_minutes++;
-		}
-		updateTimePassText();
-		resetTimer();
-}
-//------------------------------------------
-
-void StatusBar::updateTimePassText()
-{
-	if (m_seconds >= 10 && m_minutes < 10)
-		this->m_timeText.setString(" Time Pass :0" + std::to_string((int)this->m_minutes)
-			+ ":" + std::to_string((int)this->m_seconds));
-	else if (m_seconds >= 10 && m_minutes >= 10)
-		this->m_timeText.setString(" Time Pass :" + std::to_string((int)this->m_minutes)
-			+ ":" + std::to_string((int)this->m_seconds));
-	else if (m_seconds < 10 && m_minutes >= 10)
-		this->m_timeText.setString(" Time Pass :" + std::to_string((int)this->m_minutes)
-			+ ":0" + std::to_string((int)this->m_seconds));
-	else
-		this->m_timeText.setString(" Time Pass :0" + std::to_string((int)this->m_minutes)
-			+ ":0" + std::to_string((int)this->m_seconds));
-}
-//------------------------------------------
-
-void StatusBar::updateLevel( bool isLevelUp)
-{
-
-	setTime(0);
-	
+void StatusBar::updateLevel(const bool isLevelUp)
+{	
 	if (isLevelUp)
 		this->m_level++;
 
 	this->m_levelText.setString("Level :" + std::to_string(this->m_level));
-	resetTimer();
 }
 //------------------------------------------
 
@@ -72,32 +36,13 @@ int StatusBar::getLevel() const
 }
 //------------------------------------------
 
-void StatusBar::setLevel(int lev)
+void StatusBar::setLevel(const int lev)
 {
 	 this->m_level= lev;
 }
 //------------------------------------------
 
-float StatusBar::getTime() const
-{
-		return 1;
-}
-//------------------------------------------
-
-void StatusBar::resetTimer()
-{
-	m_timer.restart();
-}
-//------------------------------------------
-
-void StatusBar::setTime(int time)
-{
-	m_minutes = time/60;
-	m_seconds = time % 60;
-}
-//------------------------------------------
-
-void StatusBar::updatePos(sf::Vector2f center)
+void StatusBar::updatePos(const sf::Vector2f center)
 {
 	m_lifeFrame.setPosition(center.x + 125, center.y - 115);
 	m_currLife.setPosition(center.x +125, center.y - 115);
@@ -111,7 +56,6 @@ void StatusBar::updatePos(sf::Vector2f center)
 	m_diamondIcon.setPosition(center.x + 110, center.y - 85);
 
 	this->m_levelText.setPosition(center.x-170, center.y - 110);
-	//this->m_timeText.setPosition(center.x - 170, center.y - 100);
 	this->m_musicIcon.setPosition(center.x - 170, center.y - 90);
 	this->m_stopAndPlayIcon.setPosition(center.x -160, center.y - 90);
 }
@@ -120,37 +64,28 @@ void StatusBar::updatePos(sf::Vector2f center)
 void StatusBar::draw(sf::RenderWindow& window,const int power,
 	const int life,const int currDiamonds)
 {
-	updateTime();
 	updateDiamondText(currDiamonds);
 	updateLifeAndPower(power, life);
 
 	window.draw(this->m_diamondText);
 	window.draw(this->m_diamondIcon);
-
 	window.draw(this->m_currLife);
 	window.draw(this->m_currPower); 
 	window.draw(this->m_powerFrame);
 	window.draw(this->m_lifeFrame);
-
-
 	window.draw(this->m_powerIcon);
 	window.draw(this->m_lifeIcon);
-
 	window.draw(this->m_levelText);
 	window.draw(this->m_musicIcon);
 	window.draw(this->m_stopAndPlayIcon);
-	//std::cout<<""
 	//window.draw(this->m_resetIcon);
-	//window.draw(this->m_timeText);
-
 }
 //------------------------------------------
 
-void StatusBar::updateLifeAndPower(int power, int life)
+void StatusBar::updateLifeAndPower(const int power, const int life)
 {
 	m_currPower.setSize(sf::Vector2f(power * 0.05, m_currPower.getSize().y));
 	m_currLife.setSize(sf::Vector2f(life * 0.05, m_currLife.getSize().y));
-
 }
 //------------------------------------------
 
@@ -174,7 +109,7 @@ bool StatusBar::containsStopAndPlayIcon(const sf::Event& event) const
 }
 //------------------------------------------
 
-void StatusBar::setStopAndPlayIcon(bool ToPlay)
+void StatusBar::setStopAndPlayIcon(const bool ToPlay)
 {
 	this->m_stopAndPlayIcon.setTexture(*FileManager::instance().getPlayAndStopIcon(ToPlay));
 }
@@ -188,7 +123,7 @@ bool StatusBar::containsMusicIcon(const sf::Event& event) const
 }
 //------------------------------------------
 
-void StatusBar::setMusicIcon(bool isSoundOn)
+void StatusBar::setMusicIcon(const bool isSoundOn)
 {
 	this->m_musicIcon.setTexture(*FileManager::instance().getMusicIcon(isSoundOn));
 }
@@ -206,21 +141,9 @@ void StatusBar::setLevelText()
 {
 	this->m_levelText.setFont(*FileManager::instance().getFont());
 	this->m_levelText.setCharacterSize(STATUS_BAR_CHAR_SIZE);
-	//this->m_levelText.setPosition(WINDOW_WIDTH / 4, WINDOW_HEIGHT - BUFF_DISTANCE);
 	this->m_levelText.setColor(sf::Color(153, 153, 255, 255));
 	this->m_levelText.setOutlineColor(sf::Color(230, 230, 255, 255));
 	this->m_levelText.setOutlineThickness(STATUS_BAR_OUTLINE_THICKNESS);
-}
-//--------------------------------------------
-
-void StatusBar::setTimeText()
-{
-	this->m_timeText.setFont(*FileManager::instance().getFont());
-	this->m_timeText.setCharacterSize(STATUS_BAR_CHAR_SIZE);
-	//this->m_timeText.setPosition(WINDOW_WIDTH / 2.5, WINDOW_HEIGHT - BUFF_DISTANCE);
-	this->m_timeText.setColor(sf::Color(153, 153, 255, 255));
-	this->m_timeText.setOutlineColor(sf::Color(230, 230, 255, 255));
-	this->m_timeText.setOutlineThickness(STATUS_BAR_OUTLINE_THICKNESS);
 }
 //--------------------------------------------
 
@@ -232,7 +155,6 @@ void StatusBar::setDiamondCounter()
 	this->m_diamondText.setOutlineThickness(0.5);
 	this->m_diamondText.setOutlineColor(sf::Color::White);
 
-
 	m_diamondIcon.setTexture(*FileManager::instance().getBIcons(B_DIAMOND));
 	m_diamondIcon.scale(MUSIC_ICON_SCALE*6.f);
 }
@@ -241,7 +163,6 @@ void StatusBar::setDiamondCounter()
 void StatusBar::setMusicIcon()
 {
 	this->m_musicIcon.setTexture(*FileManager::instance().getMusicIcon(true));
-	//this->m_musicIcon.setPosition(WINDOW_WIDTH/1.3, WINDOW_HEIGHT - BUFF_DISTANCE/2);
 	this->m_musicIcon.scale(MUSIC_ICON_SCALE);
 }
 //--------------------------------------------
@@ -249,7 +170,6 @@ void StatusBar::setMusicIcon()
 void StatusBar::setStopIcon()
 {
 	this->m_stopAndPlayIcon.setTexture(*FileManager::instance().getPlayAndStopIcon(true));
-	//this->m_stopAndPlayIcon.setPosition(WINDOW_WIDTH/ 1.19, WINDOW_HEIGHT - BUFF_DISTANCE / 2);
 	this->m_stopAndPlayIcon.scale(MUSIC_ICON_SCALE);
 }
 //--------------------------------------------
@@ -257,7 +177,6 @@ void StatusBar::setStopIcon()
 void StatusBar::setRestartIcon()
 {
 	this->m_resetIcon.setTexture(*FileManager::instance().getRestartIcon());
-	//this->m_resetIcon.setPosition(WINDOW_WIDTH/1.1 , WINDOW_HEIGHT - BUFF_DISTANCE / 2);
 	this->m_resetIcon.scale(MUSIC_ICON_SCALE);
 }
 //--------------------------------------------
